d5bfdc43b51088bd25ac5ae869fbddb14b505b10
Percy Herrera Valdez <percy_j98@hotmail.com>
2022-05-25 23:19:42 -0500

    done

---
+++ MVCExample2022/Controllers/ProductController.cs
@@ -0,0 +1,100 @@
+﻿using MVCExample2022.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCExample2022.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+
+            if (Session["productos"] == null)
+            {
+                Session["productos"] = new List<ProductModel>();
+                ;
+            }
+            return View(Session["productos"]);
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+
+            var model = ((List<ProductModel>)Session["productos"]).Where(x => x.ProductID == id).FirstOrDefault();
+            return View(model);
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(ProductModel model)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                ((List<ProductModel>)Session["productos"]).Add(model);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var model = ((List<ProductModel>)Session["productos"]).Where(x => x.ProductID == id).FirstOrDefault();
+            return View(model);
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, ProductModel model)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVCExample2022/Models/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCExample2022.Models
+{
+    public class ProductModel
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+
+    }
+}
